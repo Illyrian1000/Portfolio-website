2c227b8aaed8b434bac94beb839f1d3e6e2f1a6f
Illyrian <ilirprevazi@gmail.com>
2023-02-17 20:03:16 +0100

    new name change cryptic change

--- scripts.js
+++ scripts.js
@@ -1,17 +1,29 @@
 const logo = document.querySelector(`#logo`);
 
 const nameLetters = ["I", "L", "I", "J", "A", "N"];
+const surnameLetters = ["P", "R", "E", "V", "A", "Z", "I"];
 
-const logoLetterChange = function () {
-  let i = 0;
-  setInterval(function () {
-    console.log(`interval`);
-    logo.textContent = nameLetters[i];
-    i++;
-    if (i === nameLetters.length) {
-      i = 0;
-    }
-  }, 200);
-};
+const alphabet = [];
 
-logoLetterChange();
+for (let i = 0; i < 26; i++) {
+  alphabet.push(String.fromCharCode(65 + i)); // uppercase letters A-Z
+  alphabet.push(String.fromCharCode(97 + i)); // lowercase letters a-z
+}
+
+for (let i = 0; i < 10; i++) {
+  alphabet.push(String(i)); // numbers 0-9
+}
+
+console.log(alphabet);
+
+// const logoLetterChange = function () {
+//   let i = 0;
+//   setInterval(function () {
+//     console.log(`interval`);
+//     logo.textContent = nameLetters[i];
+//     i++;
+//     if (i === nameLetters.length) {
+//       i = 0;
+//     }
+//   }, 200);
+// };
